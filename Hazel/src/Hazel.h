5a31a24c7208cacd714999d35edfcff480cab838
Ayman Elayyan <ayman.2.elayyan@gmail.com>
2024-09-29 17:25:27 +0300

    Manage includeing library

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -4,17 +4,22 @@
 #include "Hazel/Layer.h"
 #include "Hazel/Log.h"
 #include "Hazel/Core.h"
-#include "Hazel/ImGui/ImGuiLayer.h"
-
-
 #include "Hazel/Window.h"
-#include "Hazel/Events/Event.h"
-#include "Hazel/Events/ApplicationEvent.h"
-#include "Hazel/Layer.h"
 #include "Hazel/LayerStack.h"
-#include "Hazel/ImGui/ImGuiLayer.h"
 #include "Hazel/Input.h"
 #include "Hazel/KeyCodes.h"
+
+#include "Hazel/ImGui/ImGuiLayer.h"
+
+#include "Hazel/Events/Event.h"
+#include "Hazel/Events/ApplicationEvent.h"
+
+#include "Hazel/Renderer/Buffer.h"
+#include "Hazel/Renderer/RenderCommand.h"
+#include "Hazel/Renderer/Renderer.h"
+#include "Hazel/Renderer/Shader.h"
+#include "Hazel/Renderer/VertexArray.h"
+#include "Hazel/Renderer/OthographicCamera.h"
 
 
 // -----Entry Point--------------
