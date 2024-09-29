9feda15464022ff404d30ccfbb52cea03bc086d3
Ayman Elayyan <ayman.2.elayyan@gmail.com>
2024-09-29 17:23:19 +0300

    Clean Application.cpp

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,17 +1,158 @@
-#include  <Hazel/EntryPoint.h>
+#include <Hazel.h>
+#include <Hazel/EntryPoint.h>
+#include <GLFW/include/GLFW/glfw3.h>
 
 class ExampleLayer : public Hazel::Layer
 {
 public:
 	ExampleLayer()
-		: Layer("Example")
+		: m_Camera(-1.6f, 1.6f, -0.9f, 0.9f),
+		  Layer("Example")
 	{
+		m_VertexArray.reset(Hazel::VertexArray::Create());
+
+		float vertices[3 * 7] = {
+			//    x      y     z             Color
+				-0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f,
+				 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,
+				 0.0f,  0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f
+		};
+
+		std::shared_ptr<Hazel::VertexBuffer> vertexBuffer;
+		vertexBuffer.reset(Hazel::VertexBuffer::Create(vertices, sizeof(vertices)));
+		Hazel::BufferLayout layout = {
+			{ Hazel::ShaderDataType::Float3, "a_Position" },
+			{ Hazel::ShaderDataType::Float4, "a_Color" }
+		};
+		vertexBuffer->SetLayout(layout);
+		m_VertexArray->AddVertexBuffer(vertexBuffer);
+
+		uint32_t indices[3] = { 0, 1, 2 };
+		std::shared_ptr<Hazel::IndexBuffer> indexBuffer;
+		indexBuffer.reset(Hazel::IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
+		m_VertexArray->SetIndexBuffer(indexBuffer);
+
+		m_SquareVA.reset(Hazel::VertexArray::Create());
+
+		float squareVertices[3 * 4] = {
+			-0.75f, -0.75f, 0.0f,
+			 0.75f, -0.75f, 0.0f,
+			 0.75f,  0.75f, 0.0f,
+			-0.75f,  0.75f, 0.0f
+		};
+		std::shared_ptr<Hazel::VertexBuffer> squareVB;
+		squareVB.reset(Hazel::VertexBuffer::Create(squareVertices, sizeof(squareVertices)));
+		squareVB->SetLayout({
+			{ Hazel::ShaderDataType::Float3, "a_Position" }
+			});
+
+		m_SquareVA->AddVertexBuffer(squareVB);
+
+		uint32_t squareIndices[6] = { 0, 1, 2, 2, 3, 0 };
+
+		std::shared_ptr<Hazel::IndexBuffer> squareIB;
+		squareIB.reset(Hazel::IndexBuffer::Create(squareIndices, sizeof(squareIndices) / sizeof(uint32_t)));
+		m_SquareVA->SetIndexBuffer(squareIB);
+
+		std::string vertexSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
+
+			uniform mat4 u_ViewProjection;
+
+			out vec3 v_Position;
+			out vec4 v_Color;
+
+			void main()
+			{
+				v_Position = a_Position;
+				v_Color = a_Color;
+				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
+			}
+		)";
+
+		std::string fragmentSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) out vec4 color;
+
+			in vec3 v_Position;
+			in vec4 v_Color;
+			void main()
+			{
+				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
+			}
+		)";
+
+		m_Shader.reset(new Hazel::Shader(vertexSrc, fragmentSrc));
+
+		std::string blueShaderVertexSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) in vec3 a_Position;
+
+			uniform mat4 u_ViewProjection;
+
+			out vec3 v_Position;
+
+			void main()
+			{
+				v_Position = a_Position;
+				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
+			}
+		)";
+		std::string blueShaderFragmentSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) out vec4 color;
+
+			in vec3 v_Position;
+
+			void main()
+			{
+				color = vec4(0.2, 0.3, 0.8, 1.0);
+			}
+		)";
+		m_BlueShader.reset(new Hazel::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
 	}
 
 	void OnUpdate() override
 	{
-		if (Hazel::Input::IsKeyPressed(HZ_KEY_TAB))
-			HZ_TRACE("Tab key is pressed (poll)!");
+
+		Hazel::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
+		Hazel::RenderCommand::Clear();
+
+		glm::vec3 cameraPosition = m_Camera.GetPostion();
+		float rotationPosition = m_Camera.GetRotation();
+		float cameraSpeed = 0.01f;
+		float rotationSpeed = 0.4f;
+
+		if (Hazel::Input::IsKeyPressed(GLFW_KEY_S))
+			cameraPosition.y += cameraSpeed;
+		if (Hazel::Input::IsKeyPressed(GLFW_KEY_W))
+			cameraPosition.y -= cameraSpeed;
+		if (Hazel::Input::IsKeyPressed(GLFW_KEY_D))
+			cameraPosition.x -= cameraSpeed;
+		if (Hazel::Input::IsKeyPressed(GLFW_KEY_A))
+			cameraPosition.x += cameraSpeed;
+		if (Hazel::Input::IsKeyPressed(GLFW_KEY_E))
+			rotationPosition += rotationSpeed;
+		if (Hazel::Input::IsKeyPressed(GLFW_KEY_Q))
+			rotationPosition -= rotationSpeed;
+
+		m_Camera.SetPosition(cameraPosition);
+		m_Camera.SetRotation(rotationPosition);
+
+		Hazel::Renderer::BeginScene(m_Camera);
+
+		Hazel::Renderer::Submit(m_BlueShader, m_SquareVA);
+		Hazel::Renderer::Submit(m_Shader, m_VertexArray);
+
+		Hazel::Renderer::EndScene();
+
 	}
 
 
@@ -21,14 +162,17 @@ public:
 
 	void OnEvent(Hazel::Event& event) override
 	{
-		if (event.GetEventType() == Hazel::EventType::KeyPressed)
-		{
-			Hazel::KeyPressedEvent& e = (Hazel::KeyPressedEvent&)event;
-			if (e.GetKeyCode() == HZ_KEY_TAB)
-				HZ_TRACE("Tab key is pressed (event)!");
-			HZ_TRACE("{0}", (char)e.GetKeyCode());
-		}
+
 	}
+
+private:
+	std::shared_ptr <Hazel::VertexArray> m_SquareVA;
+	std::shared_ptr <Hazel::VertexArray> m_VertexArray;
+
+	std::shared_ptr <Hazel::Shader> m_BlueShader;
+	std::shared_ptr <Hazel::Shader> m_Shader;
+
+	Hazel::OthographicCamera m_Camera;
 
 };
 
