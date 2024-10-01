595ddf25c06ebea33cab4266d5e7aa7029f630c1
Ayman Elayyan <ayman.2.elayyan@gmail.com>
2024-10-01 19:31:57 +0300

    Modify HZ_INFO

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -13,8 +13,7 @@ int main(int argc, char** argv)
 	Hazel::Log::Init();
 
 	HZ_CORE_WARN("Initialized Log!");
-	int a = 5;
-	HZ_INFO("Hello! Var={0}", a);
+	HZ_INFO("Hello!");
 
 	auto app = Hazel::CreateApplication();
 	app->Run();
