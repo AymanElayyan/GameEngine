d4c4de29cefd07123484cdd8c8abb4098170cc99
Ayman Elayyan <ayman.2.elayyan@gmail.com>
2024-10-01 06:55:37 +0300

    Update Hazel.h

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -20,6 +20,7 @@
 #include "Hazel/Renderer/RenderCommand.h"
 #include "Hazel/Renderer/Renderer.h"
 #include "Hazel/Renderer/Shader.h"
+#include "Hazel/Renderer/Texture.h"
 #include "Hazel/Renderer/VertexArray.h"
 #include "Hazel/Renderer/OthographicCamera.h"
 
