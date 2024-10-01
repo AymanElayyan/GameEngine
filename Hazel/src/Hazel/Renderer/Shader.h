641548690527891a48884f9bd94146fba59b13d2
Ayman Elayyan <ayman.2.elayyan@gmail.com>
2024-10-01 16:59:10 +0300

    Implement ShaderLibrary

--- Hazel/src/Hazel/Renderer/Shader.h
+++ Hazel/src/Hazel/Renderer/Shader.h
@@ -1,5 +1,5 @@
 #pragma once 
-
+#include "Hazel/Core.h"
 #include <string>
 #include <unordered_map>
 
@@ -15,8 +15,8 @@ namespace Hazel
 		virtual void Unbind() const = 0;
 		virtual const std::string& GetName() const = 0;
 
-		static Shader* Create(const std::string& filepath);
-		static Shader* Create(const std::string& name, const std::string& vertexSrc, const std::string& fragmentSrc);
+		static Ref<Shader> Create(const std::string& filepath);
+		static Ref<Shader> Create(const std::string& name, const std::string& vertexSrc, const std::string& fragmentSrc);
 	private:
 
 	};
@@ -26,11 +26,13 @@ namespace Hazel
 	{
 	public:
 		void Add(const Ref<Shader>& shader);
+		void Add(const std::string& name, const Ref<Shader>& shader);
 
 		Ref<Shader> Load(const std::string& filepath);
 		Ref<Shader> Load(const std::string& name, const std::string& filepath);
 		Ref<Shader> Get(const std::string& name);
 
+		bool Exists(const std::string& name) const;
 	private:
 		std::unordered_map<std::string, Ref<Shader>> m_Shaders;
 
