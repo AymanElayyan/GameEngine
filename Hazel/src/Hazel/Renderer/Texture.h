963757ddc37abd306cc30027f4ef2e360e49df62
Ayman Elayyan <ayman.2.elayyan@gmail.com>
2024-10-01 06:52:40 +0300

    Set stb_image in premake5.lua

--- Hazel/src/Hazel/Renderer/Texture.h
+++ Hazel/src/Hazel/Renderer/Texture.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <Hazel.h>
 
 namespace Hazel
 {
@@ -6,6 +7,18 @@ namespace Hazel
 	{
 	public:
 
+		virtual uint32_t GetWidth() const = 0;
+		virtual uint32_t GetWidth() const = 0;
 
+		virtual void Bind() const = 0;
+	};
+
+
+
+	class Texture2D : public Texture
+	{
+	public:
+		static Ref<Texture2D> Create(const std::string& path);
+		
 	};
 }
