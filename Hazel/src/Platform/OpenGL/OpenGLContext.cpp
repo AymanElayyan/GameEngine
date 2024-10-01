06cf4e1657f86dbb99e5327d221fad4f6adde66e
Ayman Elayyan <ayman.2.elayyan@gmail.com>
2024-10-01 19:38:52 +0300

    Inhance OpenGL Info

--- Hazel/src/Platform/OpenGL/OpenGLContext.cpp
+++ Hazel/src/Platform/OpenGL/OpenGLContext.cpp
@@ -11,6 +11,7 @@ namespace Hazel
 	OpenGLContext::OpenGLContext(GLFWwindow* windowHandle)
 		: m_WindowHandle(windowHandle)
 	{
+		
 		HZ_CORE_ASSERT(windowHandle, "Handle handle is null!")
 	} 
 
@@ -21,11 +22,11 @@ namespace Hazel
 		HZ_CORE_ASSERT(status, "Failed to initialize Glad!");
 	
 		HZ_CORE_INFO("OpenGL Info: ");
-		HZ_CORE_INFO(" {");
-		HZ_CORE_INFO("   Vendor: {0}", (const char*)glGetString(GL_VENDOR));
-		HZ_CORE_INFO("   Renderer: {0}", (const char*)glGetString(GL_RENDERER));
-		HZ_CORE_INFO("   Version: {0}", (const char*)glGetString(GL_VERSION));
-		HZ_CORE_INFO(" }");
+		HZ_CORE_INFO("{");
+		HZ_CORE_INFO("  Vendor: {0}", (const char*)glGetString(GL_VENDOR));
+		HZ_CORE_INFO("  Renderer: {0}", (const char*)glGetString(GL_RENDERER));
+		HZ_CORE_INFO("  Version: {0}", (const char*)glGetString(GL_VERSION));
+		HZ_CORE_INFO("}");
 	}
 
 	void OpenGLContext::SwapBuffers()
