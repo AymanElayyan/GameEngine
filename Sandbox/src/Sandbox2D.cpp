f6a7a0a88e29a68ca448a4bc6e282766dd6720bf
Ayman Elayyan <ayman.2.elayyan@gmail.com>
2024-10-02 19:58:14 +0300

    Fix Upload Uniform Float 3 to 4

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -55,7 +55,7 @@ void Sandbox2D::OnUpdate(Hazel::Timestep ts)
 	Hazel::Renderer::BeginScene(m_CameraController.GetCamera());
 
 	std::dynamic_pointer_cast<Hazel::OpenGLShader> (m_FlatColorShader)->Bind();
-	std::dynamic_pointer_cast<Hazel::OpenGLShader> (m_FlatColorShader)->UploadUniformFloat3("u_Color", m_SquareColor);
+	std::dynamic_pointer_cast<Hazel::OpenGLShader> (m_FlatColorShader)->UploadUniformFloat4("u_Color", m_SquareColor);
 	
 	Hazel::Renderer::Submit(m_FlatColorShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
 
