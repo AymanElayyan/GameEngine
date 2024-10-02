3b16c90c25ba6be3c6d1bcf995acc5c2e327fabd
Ayman Elayyan <ayman.2.elayyan@gmail.com>
2024-10-03 01:13:03 +0300

    Implement DrawQuad function

--- Hazel/src/Hazel/Renderer/Renderer2D.cpp
+++ Hazel/src/Hazel/Renderer/Renderer2D.cpp
@@ -77,7 +77,7 @@ namespace Hazel
 	{
 		std::dynamic_pointer_cast<OpenGLShader> (s_Data->FlatColorShader)->Bind();
 		std::dynamic_pointer_cast<Hazel::OpenGLShader> (s_Data->FlatColorShader)->UploadUniformFloat4("u_Color", color);
-		
+
 		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) * glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
 		s_Data->FlatColorShader->SetMat4("u_Translate", transform);
 
@@ -87,17 +87,13 @@ namespace Hazel
 
 	void Renderer2D::DrawQuad(const glm::vec2& position, const glm::vec2& size, const Ref<Texture2D>& texture)
 	{
-		DrawQuad(
-			{ position.x, position.y, 0.0f },
-			size,
-			texture
-		);
+		DrawQuad({ position.x, position.y, 0.0f }, size, texture);
 	}
 	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const Ref<Texture2D>& texture)
 	{
 		std::dynamic_pointer_cast<OpenGLShader> (s_Data->FlatColorShader)->Bind();
-		
-		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) * glm::scale(glm::mat4(1.0f), {size.x, size.y, 1.0f});
+
+		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) * glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
 		s_Data->FlatColorShader->SetMat4("u_Translate", transform);
 
 		texture->Bind();
