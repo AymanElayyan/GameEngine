198cb2b8d58daf1a5992665483d1227690a7075b
Ayman Elayyan <ayman.2.elayyan@gmail.com>
2024-10-03 20:20:59 +0300

    VisualProfiling for Runtime.json

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -6,50 +6,6 @@
 
 #include <glm/gtc/type_ptr.hpp>
 
-#include <chrono>
-
-template <typename Fn>
-class Timer
-{
-public:
-	Timer(const char* name, Fn&& function)
-		: m_Name(name), m_Stopped(false), m_Func(function)
-	{
-		m_StartTimepoint = std::chrono::high_resolution_clock::now();
-	}
-
-	~Timer()
-	{
-		if (!m_Stopped)
-			Stop();
-	}
-
-	void Stop()
-	{
-		auto endTimepoint = std::chrono::high_resolution_clock::now();
-
-		long long start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
-		long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
-
-		m_Stopped = true;
-
-		float duration = (end - start) * 0.001f;
-		m_Func({m_Name, duration});
-
-	}
-
-
-private:
-	const char* m_Name;
-	std::chrono::time_point<std::chrono::steady_clock> m_StartTimepoint;
-	bool m_Stopped;
-	Fn m_Func;
-};
-
-#define PROFILE_SCOPE(name) Timer timer##__LINE__(name, [&](ProfilResults profilResult) {m_ProfilResult.push_back(profilResult); })
-
-
-
 Sandbox2D::Sandbox2D()
 	:Layer("SandBox2D"), m_CameraController(1288.0f / 720.0f, true)
 {
@@ -67,21 +23,21 @@ void Sandbox2D::OnDetach()
 
 void Sandbox2D::OnUpdate(Hazel::Timestep ts)
 {
-	PROFILE_SCOPE("Sandbox::OnUpdate");
+	HZ_PROFILE_FUNCTION();
 	
 	{
-		PROFILE_SCOPE("CameraController::OnUpdate");
+		HZ_PROFILE_SCOPE("CameraController::OnUpdate");
 		m_CameraController.OnUpdate(ts);
 	}
 
 	{
-		PROFILE_SCOPE("Renderer Prep");
+		HZ_PROFILE_SCOPE("Renderer Prep");
 		Hazel::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
 		Hazel::RenderCommand::Clear();
 	}
 	
 	{
-		PROFILE_SCOPE("Renderer Draw");
+		HZ_PROFILE_SCOPE("Renderer Draw");
 		Hazel::Renderer2D::BeginScene(m_CameraController.GetCamera());
 		Hazel::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.3f, 0.5f, 0.7f, 1.0f });
 		Hazel::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.8f }, { 0.4f, 0.1f, 0.5f, 1.0f });
@@ -92,17 +48,10 @@ void Sandbox2D::OnUpdate(Hazel::Timestep ts)
 
 void Sandbox2D::OnImGuiRender()
 {
+	HZ_PROFILE_FUNCTION();
+
 	ImGui::Begin("Setting");
 	ImGui::ColorEdit4("Sequre Color", glm::value_ptr(m_SquareColor));
-
-	for (auto& result : m_ProfilResult)
-	{
-		char label[50];
-		strcpy(label, "%.3fms  ");
-		strcat(label, result.Name);
-		ImGui::Text(label, result.Time);
-	}
-	m_ProfilResult.clear();
 
 	ImGui::End();
 }
--- Sandbox/src/Sandbox2D.h
+++ Sandbox/src/Sandbox2D.h
@@ -23,14 +23,6 @@ private:
 	Hazel::Ref <Hazel::Shader> m_FlatColorShader;
 	Hazel::Ref <Hazel::Texture2D> m_CheckerboardTexture;
 
-	struct ProfilResults
-	{
-		const char* Name;
-		float Time;
-	};
-
-	std::vector<ProfilResults> m_ProfilResult;
-
 	glm::vec4 m_SquareColor = { 0.2f, 0.3f, 0.8f, 1.0f };
 
 };
