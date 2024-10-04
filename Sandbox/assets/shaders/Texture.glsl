a7a34e65f8697253bab9bceeaf08c14e0feb90dc
Ayman Elayyan <ayman.2.elayyan@gmail.com>
2024-10-04 17:16:43 +0300

    Add TexIndex in Texture.glsl

--- Sandbox/assets/shaders/Texture.glsl
+++ Sandbox/assets/shaders/Texture.glsl
@@ -4,18 +4,21 @@
 layout(location = 0) in vec3 a_Position;
 layout(location = 1) in vec4 a_Color;
 layout(location = 2) in vec2 a_TexCoord;
+layout(location = 3) in float a_TexIndex;
 
 uniform mat4 u_ViewProjection;
 
-out vec2 v_TexCoord;
 out vec4 v_Color;
+out vec2 v_TexCoord;
+out float v_TexIndex;
 
 void main()
 {
-	v_Color = a_Color;
-	v_TexCoord = a_TexCoord;
+	v_Color	    = a_Color;
+	v_TexCoord  = a_TexCoord;
+	v_TexIndex  = a_TexIndex;
 	gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
-}		
+}			
 		
 #type fragment
 #version 330 core
@@ -24,6 +27,7 @@ layout(location = 0) out vec4 color;
 
 in vec4 v_Color;
 in vec2 v_TexCoord;
+in float v_TexIndex;
 
 uniform vec4 u_Color;
 uniform float u_TilingFactor;
