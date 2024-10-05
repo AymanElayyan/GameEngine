853b404a0faf1b361c1002321dcb3f7ba4803d50
Ayman Elayyan <ayman.2.elayyan@gmail.com>
2024-10-05 18:16:16 +0300

    Add class Random

--- Sandbox/src/Particle.cpp
+++ Sandbox/src/Particle.cpp
@@ -1,9 +1,26 @@
-#include "ParticleSystem.h"
-
-#include "Random.h"
+#include "Particle.h"
 
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/gtx/compatibility.hpp>
+
+#include <random>
+
+class Random
+{
+public:
+	static void Init()
+	{
+		s_RandomEngine.seed(std::random_device()());
+	}
+
+	static float Float()
+	{
+		return (float)s_Distribution(s_RandomEngine) / (float)std::numeric_limits<uint32_t>::max();
+	}
+private:
+	static std::mt19937 s_RandomEngine;
+	static std::uniform_int_distribution<std::mt19937::result_type> s_Distribution;
+};
 
 ParticleSystem::ParticleSystem()
 {
