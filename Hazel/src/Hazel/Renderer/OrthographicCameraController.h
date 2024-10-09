a2d0508199054f566f318ac663da1f7e37a1f7d1
Ayman Elayyan <ayman.2.elayyan@gmail.com>
2024-10-09 21:51:49 +0300

    Create CalculateView() and use it in SetZoomLevel()

--- Hazel/src/Hazel/Renderer/OrthographicCameraController.h
+++ Hazel/src/Hazel/Renderer/OrthographicCameraController.h
@@ -29,12 +29,14 @@ namespace Hazel {
 		const OrthographicCamera& GetCamera() const { return m_Camera; }
 
 		float GetZoomLevel() const { return m_ZoomLevel; }
-		void SetZoomLevel(float level) { m_ZoomLevel = level; }
+		void SetZoomLevel(float level) { m_ZoomLevel = level; CalculateView(); }
 		const OrthographicCameraBounds& GetBounds() const { return m_Bounds; }
 
 	private:
+		void CalculateView();
 		bool OnMouseScrolled(MouseScrolledEvent& e);
 		bool OnWindowResized(WindowResizeEvent& e);
+	
 	private:
 		float m_AspectRatio;
 		float m_ZoomLevel = 1.0f;
