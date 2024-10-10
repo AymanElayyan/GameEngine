71773e4637e1c1496c7078dd2951a5b84a095a5c
Ayman Elayyan <ayman.2.elayyan@gmail.com>
2024-10-10 10:50:32 +0300

    Craete OpenGL API for OpenGLFramebuffer

--- Hazel/src/Platform/OpenGL/OpenGLFramebuffer.h
+++ Hazel/src/Platform/OpenGL/OpenGLFramebuffer.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#include "Hazel/Renderer/Framebuffer.h"
+
+namespace Hazel
+{
+	class OpenGLFrameBuffer : public Framebuffer
+	{
+	public:
+
+	private:
+		FramebufferSpecification m_Specification;
+
+	};
+}
