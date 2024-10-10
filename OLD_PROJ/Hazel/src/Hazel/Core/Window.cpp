4462128161cce74a81aa5f7f5557c6e25380344d
Ayman Elayyan <ayman.2.elayyan@gmail.com>
2024-10-10 12:12:13 +0300

    Delete OLD_PROJ

--- OLD_PROJ/Hazel/src/Hazel/Core/Window.cpp
+++
@@ -1,9 +0,0 @@
-#include "Hzpch.h"
-#include "Window.h"
-#include "Platform/Windows/WindowsWindow.h"
-
-namespace Hazel {
-    Window* Window::Create(const WindowProps& props) {
-        return new WindowsWindow(props); 
-    }
-}
