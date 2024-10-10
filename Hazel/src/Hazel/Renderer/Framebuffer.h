cfe9a9076630dd32f485720b4b73e6fc43099e32
Ayman Elayyan <ayman.2.elayyan@gmail.com>
2024-10-10 23:13:34 +0300

    Init Resize function

--- Hazel/src/Hazel/Renderer/Framebuffer.h
+++ Hazel/src/Hazel/Renderer/Framebuffer.h
@@ -17,7 +17,8 @@ namespace Hazel
 	public:
 		virtual void Bind() = 0;
 		virtual void Unbind() = 0;
-
+		virtual void Resize(uint32_t Width, uint32_t Height) = 0;
+		
 		virtual uint32_t GetColorAttachmentRendererID() const = 0;
 
 		virtual const FramebufferSpecification& GetSpecification() const = 0;
